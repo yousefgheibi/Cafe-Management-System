86f0c1d1bddd806ee6dc092d5342f0319c25e7a3
yousef <gheibiyousef2000@gmail.com>
2022-04-13 20:10:33 +0430

    - Add, Get & Update API of Category

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@
    const cors = require('cors');
    const connection = require("./connection");
    const userRoute = require("./routes/user");
+   const categoryRoute = require("./routes/category");
    const app =  express();
 
    app.use(cors());
    app.use(bodyParser.json());
 
    app.use('/user',userRoute);
- 
+   app.use('/category',categoryRoute);
+
    module.exports = app;
---
+++ backend/routes/category.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const connection = require("../connection");
+const router = express.Router();
+var auth = require("../services/authentication");
+var checkRole = require("../services/checkRole");
+
+
+router.post('/add',auth.authenticateToken, checkRole.checkRole,(req,res,next)=>{
+    let catrgory = req.body;
+    let query = `insert into category (name) values(?)`;
+    connection.query(query,[catrgory.name],(err,result)=>{
+        if(!err){
+            return res.status(200).json({message: "Category Added Successfully"});
+        }
+        else{
+            return res.send(500).json(err);
+        }
+    })
+})
+
+
+router.get('/get',auth.authenticateToken,checkRole.checkRole,(req,res,next)=>{
+    let query = "select * from category order by name";
+    connection.query(query,(err,result)=>{
+        if(!err){
+            return res.status(200).json(result);
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+})
+
+
+router.patch('/update',auth.authenticateToken,checkRole.checkRole,(req,res,next)=>{
+    let product = req.body;
+    let query = "update category set name=? where id=?";
+    connection.query(query,[product.name,product.id],(err,result)=>{
+        if(!err){
+            if(result.affectedRows == 0){
+                return res.status(404).json({message : "Category id does not found"});
+            }
+            return res.status(200).json({message:"Category Updated Successfully"});
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+})
+
+
+ module.exports = router;
